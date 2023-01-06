16a9778d837c941f00a4b62ae785421ec7b42911
Shalale <selale.memmedli11@gmail.com>
2023-01-06 18:56:24 +0400

    ttgitignowe file added"
    This reverts commit f33ff2ca54f755507371c655c18a1b29a18c1ac3.

--- src/main/java/com/example/springbootcache/CountryRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.springbootcache;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CountryRepository extends JpaRepository<CountryEntity, Long> {
-}
